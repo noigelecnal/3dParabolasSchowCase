57931ec1c7c614a804dfa9939366a134a7a55541
noigelecnal <pol.txt.exe@gmail.com>
2025-07-05 20:44:14 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ init();
 function init() {
   scene = new THREE.Scene();
   camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-  camera.position.set(0, 2, 5);
+  camera.position.set(0, 0, 0);
 
   renderer = new THREE.WebGLRenderer({antialias: true});
   renderer.setSize(window.innerWidth, window.innerHeight);
